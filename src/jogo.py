919143123d107bff84f9539ce04ba21155d43bab
Nícolas Hecker Silva <nicolashecker.nhs@gmail.com>
2025-05-31 10:34:36 -0300

    add esqueleto

---
+++ src/jogo.py
@@ -0,0 +1,19 @@
+
+
+
+class Jogo():
+
+    def __init__(self):
+        pass
+
+    def tick(self):
+        """Atualiza o estado do jogo."""
+        pass
+
+    def render(self, motor):
+        """Renderiza o jogo na tela."""
+        pass
+
+    def input(self, event):
+        """Processa a entrada do usuário."""
+        pass
